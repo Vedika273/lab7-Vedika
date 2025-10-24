ff7ad9362015fa68aef779cc39a2383119c05950
Vedika894 <vjain894@yahoo.com>
2025-10-24 19:42:18 -0400

    all the code I typed in class

---
+++ src/lab7vedika/Lab7Vedika.java
@@ -0,0 +1,94 @@
+package lab7vedika;
+
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+/**
+ *
+ * @author 6303129
+ */
+public class Lab7Vedika extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        
+        Pane pane= new Pane();
+        
+        //animation 1
+        Rectangle rectangle = new Rectangle(100, 100, 300, 300);
+        rectangle.setFill(Color.WHITE);
+        rectangle.setStroke(Color.BLACK);
+        Circle circle = new Circle(125, 100, 50);
+        circle.setFill(Color.ORANGE);
+        circle.setStroke(Color.BLACK);
+        // Create a path transition
+        PathTransition pt = new PathTransition();
+        pt.setDuration(Duration.millis(4000));
+        pt.setPath(rectangle);
+        pt.setNode(circle);
+        pt.setOrientation(PathTransition.OrientationType.
+        ORTHOGONAL_TO_TANGENT);
+        pt.setCycleCount(Timeline.INDEFINITE);
+        pt.setAutoReverse(false);
+        pt.play(); // Start animation
+        circle.setOnMousePressed(e -> pt.pause());
+        circle.setOnMouseReleased(e -> pt.play());
+    
+        
+        //create object B 
+        Ellipse ellipse = new Ellipse();
+        ellipse.setCenterX(250);
+        ellipse.setCenterY(250);
+        ellipse.setRadiusX(60);
+        ellipse.setRadiusY(20);
+        ellipse.setFill(null);
+        ellipse.setStroke(Color.BLACK);
+        
+        ellipse.setFill(Color.RED); 
+        ellipse.setStroke(Color.BLACK);
+        ellipse.centerXProperty().bind(pane.widthProperty().divide(2)); 
+        ellipse.centerYProperty().bind(pane.heightProperty().divide(2)); 
+        ellipse.radiusXProperty().bind(pane.widthProperty().multiply(0.4)); 
+        ellipse.radiusYProperty().bind(pane.heightProperty().multiply(0.4)); 
+        pane.getChildren().add(ellipse);
+        // Apply a fade transition to ellipse
+        FadeTransition ft = new FadeTransition(Duration.millis(3000), ellipse); 
+        ft.setFromValue(1.0);
+        ft.setToValue(0.1); 
+        ft.setCycleCount(Timeline.INDEFINITE); 
+        ft.setAutoReverse(true);
+        ft.play(); // Start animation
+        // Control animation
+        ellipse.setOnMousePressed(e -> ft.pause()); 
+        ellipse.setOnMouseReleased(e -> ft.play());
+        
+        pane.getChildren().addAll(circle,rectangle, ellipse);
+        
+        //SequentialTransition seq = new Sequiential 
+        
+        Scene scene = new Scene(pane, 500, 500);
+        stage.setTitle("PathTransitionDemo");
+        stage.setScene(scene);
+        stage.show();
+ }
+}
